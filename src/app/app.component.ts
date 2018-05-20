c4399093fa5db46464b03aeb7d4e46f298c590ef
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-20 20:07:11 +0530

    predefined pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,6 @@ export class AppComponent {
   private var_three:Date= new Date();
   private var_four:Array<any>=[10,20,30,40,50];
   private var_five:number=0.9;
+  private var_six:number=4.404;
+  private var_seven:any={p_id:111,p_name:'p_one',p_cost:10000};
 }
